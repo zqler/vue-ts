c55fb186560dbed5922c5baf31b2b14b885191e9
zhangquan <434651406@qq.comm>
2020-04-08 19:24:03 +0800

    第一个提交

---
+++ src/views/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+<script lang="ts">
+// vue-property-decorator 是在vue-class-component 上增强了更多结合vue 特性的装饰器，新增了这个7个装饰器
+// @emit @Inject @Model @Prop @Provide @watch @Component(从vue-class-component继承)
+  import { Component, Emit, Inject, Model, Prop, Provide, Vue, Watch } from 'vue-property-decorator'
+  @Component
+  export default class About extends Vue {
+   @Prop()
+   propA: number=1
+
+   @Watch('child')
+    onChildChange(val: string, oldval: string):void {}
+  }
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <span @click="onClick">点击</span>
+  </div>
+</template>
+
+<script lang="ts">
+// @ is an alias to /src
+import Vue from 'vue'
+import HelloWorld from '@/components/HelloWorld.vue'
+import Component from 'vue-class-component'
+// 推荐使用标准类写，不用语法糖
+
+@Component({
+  components: {
+    HelloWorld
+  }
+})
+export default class Home extends Vue {
+    //初始数据可以直接声明为实例的属性 
+    // initial data
+     message: string = 'HelloWorld,every body';
+     // mounted
+     mounted(){
+       this.onPrint();
+     }
+     // computed
+      get computedMsg () {
+        return 'computed ' + this.message
+       }
+     // methods
+      onClick (): void {
+         // window.alert(this.message)
+       this.$router.push('About')
+     }
+     onPrint():void{
+       window.alert(this.message)
+     }
+     callbackMessage(message:string){
+       console.log(message)
+     }
+}
+</script>
